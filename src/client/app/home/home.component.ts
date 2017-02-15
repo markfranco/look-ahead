64fa229f4fd702e647d30d07dbe4273f16acf8c8
Mark Franco <mark.f@prospa.com>
2017-02-16 02:12:31 +1100

    Removing name list service

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NameListService } from '../shared/name-list/name-list.service';
-
 import { AusPostService } from '../shared/aus-post.service';
 
 @Component({
--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { NameListService } from '../shared/name-list/name-list.service';
+import { AusPostService } from '../shared/aus-post.service';
 
 @NgModule({
   imports: [CommonModule, HomeRoutingModule, SharedModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
-  providers: [NameListService]
+  providers: [AusPostService]
 })
 export class HomeModule { }
